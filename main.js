b0c000176a35dd4c8a55d24cba14f5d36b0ea87b
Romano <romanomanfredmantek@gmail.com>
2022-06-13 15:20:30 +0200

    eingedeutscht klassen geändert footerbox hinzugefügt aber noch leer

--- main.js
+++ main.js
@@ -30,15 +30,15 @@ list.addEventListener('click', function(ev) {
 // Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
+  var inputValue = document.getElementById("eingabeText").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
-    alert("You must write something!");
+    alert("Digga! Du musst schon was schreiben!");
   } else {
-    document.getElementById("myUL").appendChild(li);
+    document.getElementById("doToListe").appendChild(li);
   }
-  document.getElementById("myInput").value = "";
+  document.getElementById("eingabeText").value = "";
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
