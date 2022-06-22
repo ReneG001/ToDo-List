9cdbbc6b8da7f857f339128278b3fbde69ddb9be
Sarah Bühler <97050915+SarahBuehler@users.noreply.github.com>
2022-06-22 11:37:48 +0200

    moved button functions into newElement()

--- main.js
+++ main.js
@@ -7,46 +7,6 @@ if (Array.isArray(loadedToDos)) {
   }
 }
 
-// Mit dem Löschen Knopf, löschst du den Eintrag
-var close = document.getElementsByClassName("close");
-// console.log(close);
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function () {
-    var div = this.parentElement;
-    // bisher wurde nur der li-Punkt unsichtbar gemacht
-    // div.style.display = "none";
-    // mit der folgenden Zeile wird der li-Punkt gelöscht ganz gelöscht
-    // console.log(div); // gibt li aus
-    // console.log(div.firstChild.innerText);
-    const textDelete = div.firstChild.innerText;
-
-    // es fehlt noch das Löschen aus dem Array für den local storage
-    // Funktionsaufruf mit Element, das gelöscht werden soll
-    deleteFromLocalStorage(textDelete);
-    div.parentNode.removeChild(div);
-  };
-}
-
-// Streicht den Text durch, um die Aufgabe als erledigt zu markieren
-var listTodos = document.getElementsByClassName("contentText");
-//console.log(listTodos);
-
-for (let k = 0; k < listTodos.length; k++) {
-  listTodos[k].onclick = function (ev) {
-    const listElementTitel = ev.target;
-    console.log(listTodos);
-    // wenn noch kein "line-through" gesetzt ist, dann setzt eins
-    if (listElementTitel.style.textDecoration != "line-through") {
-      listElementTitel.style.textDecoration = "line-through";
-    }
-    // mit "checked" wird der Haken vorne gesetzt
-    if (listElementTitel.className != "checked") {
-      listElementTitel.className = "checked";
-    }
-  };
-}
-
 // Macht neuen Listeneintrag wenn du auf Hinzufügen klickst
 function newElement(inputValue) {
   var li = document.createElement("li");
@@ -72,16 +32,56 @@ function newElement(inputValue) {
   span.appendChild(txt);
   li.appendChild(span);
   saveToLocalStorage();
-}
 
-// Mit dem Ändern Knopf erscheint ein hidden Textfeld und Hinzufügen-Knopf
-const changeButton = document.getElementsByClassName("change");
-// console.log(changeButton);
-// Erst mal mit prompt
-for (let j = 0; j < changeButton.length; j++) {
-  changeButton[j].onclick = function () {
-    alert(`Button ${changeButton[j]} was clicked.`);
-  };
+  // Streicht den Text durch, um die Aufgabe als erledigt zu markieren
+  var listTodos = document.getElementsByClassName("contentText");
+  //console.log(listTodos);
+
+  for (let k = 0; k < listTodos.length; k++) {
+    listTodos[k].onclick = function (ev) {
+      const listElementTitel = ev.target;
+      console.log(listTodos);
+      // wenn noch kein "line-through" gesetzt ist, dann setzt eins
+      if (listElementTitel.style.textDecoration != "line-through") {
+        listElementTitel.style.textDecoration = "line-through";
+      }
+      // mit "checked" wird der Haken vorne gesetzt
+      if (listElementTitel.className != "checked") {
+        listElementTitel.className = "checked";
+      }
+    };
+  }
+
+  // Mit dem Löschen Knopf, löschst du den Eintrag
+  var close = document.getElementsByClassName("close");
+  // console.log(close);
+  var i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      // bisher wurde nur der li-Punkt unsichtbar gemacht
+      // div.style.display = "none";
+      // mit der folgenden Zeile wird der li-Punkt gelöscht ganz gelöscht
+      // console.log(div); // gibt li aus
+      // console.log(div.firstChild.innerText);
+      const textDelete = div.firstChild.innerText;
+
+      // es fehlt noch das Löschen aus dem Array für den local storage
+      // Funktionsaufruf mit Element, das gelöscht werden soll
+      deleteFromLocalStorage(textDelete);
+      div.parentNode.removeChild(div);
+    };
+  }
+
+  // Mit dem Ändern Knopf erscheint ein hidden Textfeld und Hinzufügen-Knopf
+  const changeButton = document.getElementsByClassName("change");
+  // console.log(changeButton);
+  // Erst mal mit prompt
+  for (let j = 0; j < changeButton.length; j++) {
+    changeButton[j].onclick = () => {
+      alert(`Button ${changeButton[j]} was clicked.`);
+    };
+  }
 }
 
 /* var j;
@@ -99,7 +99,9 @@ for (j = 0; j < changeButton.length; j++) {
 } */
 
 // Funktion für Ändernbutton
-function newText() {}
+function newText() {
+  alert("clicked");
+}
 
 /* Local storage */
 
@@ -117,7 +119,8 @@ function saveToLocalStorage() {
 
 // wenn ein Element gelöscht wird, soll es aus dem local storage entfernt werden
 function deleteFromLocalStorage(textDelete) {
-  /*   console.log("was ist im local storage: " + loadedToDos);
+  /*   
+  console.log("was ist im local storage: " + loadedToDos);
   console.log("was soll gelöscht werden: " + textDelete);
   console.log("Länge des local storage: " + loadedToDos.length);
   console.log("position von textDelete " + loadedToDos.indexOf(textDelete)); */
